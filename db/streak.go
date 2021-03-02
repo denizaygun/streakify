e6e01b8aa07d3e887a09167064837ca820bfa66b
denizaygun <me@denizaygun.co.uk>
2021-03-02 21:40:51 +0000

    refactor: minor updates

--- db/streak.go
+++ db/streak.go
@@ -3,7 +3,7 @@ package db
 import (
 	"database/sql"
 
-	"../models"
+	"github.com/denizaygun/streakify/models"
 )
 
 // GetAllStreaks ...
--- handler/handler.go
+++ handler/handler.go
@@ -18,11 +18,13 @@ func NewHandler(db db.Database) http.Handler {
 	router.Route("/streaks", streaks)
 	return router
 }
+
 func methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(405)
 	render.Render(w, r, ErrMethodNotAllowed)
 }
+
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(400)
